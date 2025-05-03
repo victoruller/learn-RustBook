a54a57a52d5b916948bc000d1452deae296a679c
victoruller <victoruller12@gmail.com.br>
2025-05-03 17:37:06 -0300

    Atualizações

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    // Sair do escopo
+// Exclusão n°1: saiu do escopo
     {
         let _s: String = String::from("String propriamente dita");
     }
@@ -11,18 +11,33 @@ fn main() {
     }
     //    println!("{_s} é inválido aqui fora e o valor apontado por ele já foi excluído"); // <- erro de compilação
 
-    // -- Reatribuição de Owner
+
+// Exclusão N°2: Reatribuição de Owner
     let mut _real_string: String = String::from("String propriamente dita");
     _real_string = String::from("Valor diferente");
     // O valor "String propriamente dita" apontado anteriormente já foi excluido automaticamente da memória
 
     //Em outras linguagens ele ficaria esquecido.
+    
+
+    
+    let mut _real_string: String = String::from("Fora");
+    {
+        _real_string = String::from("Dentro");
+    }
+    println!("O valor é: {}", _real_string); // Dentro
+
+    {
+        let _real_string = String::from("Dentro Dentro");
+    }
+    println!("O valor 2 é: {}", _real_string); // Dentro
 
 
 
 
 
-// BORROW SITUATIONS
+
+// Exclusão Nº3: BORROW SITUATIONS
 
     // -- Situação 1 (OK) -- Borrow com variável de tamanho fixo.
     let _litera_int: u32 = 10;
@@ -30,6 +45,7 @@ fn main() {
     let _x: u32 = _litera_int;
 
     println!("{_litera_int} ainda pode ser usado (e {_x} também)"); // Variáveis de tamaho fixo tem `copy` implementado, que possibilita isso.
+
 
 
 
@@ -47,6 +63,7 @@ fn main() {
 
 
 
+
     // -- Situação 3 (ERRO) -- Borrow de owner
     let _real_string: String = String::from("String propriamente dita"); // String pode modificar seu tamanho, visto ser um tipo complexo;
 
@@ -58,7 +75,8 @@ fn main() {
 
 
 
-    // -- Situação 4 (ERRO) -- Borrow de ownercomo parâmetro para função
+
+    // -- Situação 4 (ERRO) -- Borrow de owner como parâmetro para função
     let _real_string: String = String::from("String propriamente dita");
 
     function(_real_string);
@@ -72,8 +90,30 @@ fn main() {
 
 
 
+    // -- Situação 5 (ERRO) -- Shaowing o owner
 
-    // -- Situalção 5 (OK) -- Usando clone()
+    let _real_string: String = String::from("String propriamente dita");
+    let _real_string = 13; // A string do owner foi deletada
+    // para provar, crie um objeto com println na função drop()
+
+    let real_string: String = String::from("String propriamente dita");
+    let pointer = &real_string;
+    let _real_string = 12; // A string continua em memória, pois está viva por meio do pointer
+
+    println!("Dentro do pointer há: {}", pointer);
+
+
+    
+
+
+
+    // -- Situação 7 (OK) -- Usando Referência
+    let _real_string : String = String::from("String propriamente dita");
+    let _x = &_real_string;
+    println!("Referência não exclui o owner: {_real_string}");
+
+
+    // -- Situalção 8 (OK) -- Usando clone()
     let _real_string: String = String::from("String propriamente dita"); // String pode modificar seu tamanho, visto ser um tipo complexo;
 
     let _x: String = _real_string.clone();
